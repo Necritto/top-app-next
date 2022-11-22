ad2bb5aac16577a2c7390b486706ecf6af321f53
Necritto <onetojj@gmail.com>
2022-11-22 22:24:33 +0300

    refactor: eslint fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import type {AppProps} from 'next/app';
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Top App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function Home(): React.ReactElement {
     return (
-      <div>
+        <div>
         Hello
-      </div>
-  );
+        </div>
+    );
 }
