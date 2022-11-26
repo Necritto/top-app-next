2bfbf1616a7fe4763aba2a269806a4456e170666
Necritto <onetojj@gmail.com>
2022-11-26 22:08:50 +0300

    feat: setting up linters and types

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: "all",
+    tabWidth: 4,
+    printWidth: 120,
+    endOfLine: "lf",
+};
--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
 
+        config.plugins.push(new StylelintPlugin());
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
